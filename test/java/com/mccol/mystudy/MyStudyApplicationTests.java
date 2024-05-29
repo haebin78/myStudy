1bf5c8e5868a94bd29f198f722fcee6cdede15c9
해빈 <2haebin78@gmail.com>
2024-05-29 12:18:51 +0900

    처음 커밋

---
+++ test/java/com/mccol/mystudy/MyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccol.mystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
